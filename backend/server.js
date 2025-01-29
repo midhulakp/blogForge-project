0677890b282b8d7df44f871349c7bc552278a2a3
Midhula K P <kpmidhula7@gmail.com>
2025-01-29 10:28:25 +0530

    intial set up folders and add backend files and readme

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import http from "http";
+import app from "./app.js";
+
+let server = http.createServer(app);
+let PORT = process.env.PORT;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
