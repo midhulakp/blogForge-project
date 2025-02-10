0d1c2a89df1329d2815fe398828be6da2759198c
Midhula K P <kpmidhula7@gmail.com>
2025-02-10 12:13:22 +0530

    add check roles and error handling in production

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,24 @@
 import express from "express";
-
+import db from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import blogRoutes from "./routes/blogRoutes.js";
+db();
 let app = express();
+
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
+app.use("/api/blog", blogRoutes);
+
+//global error handler
+app.use((err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Something went wrong!!Please try again Later";
+  return res.status(statusCode).json({
+    status: "Failure",
+    message,
+    stack: process.env.NODE_ENV === "production" ? undefined : err.stack,
+  });
+});
 
 export default app;
